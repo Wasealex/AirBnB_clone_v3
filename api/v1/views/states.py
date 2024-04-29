1a139814786b429b7483e5a78e07c8ea0a2eb5cc
ethioendu <117819326+ethioendu@users.noreply.github.com>
2024-04-29 21:02:52 +0300

    Update states.py pycodestyle

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -23,7 +23,7 @@ def get_one_state(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', 
+@app_views.route('/states/<state_id>',
                  methods=['DELETE'], strict_slashes=False)
 def delete_state(state_id):
     state = storage.get("State", state_id)
