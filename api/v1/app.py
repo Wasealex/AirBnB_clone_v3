25655ff4d63a756b01ec22a528c0f04935d4945c
Wasealex <wasealex27@gmail.com>
2024-04-29 12:25:30 +0300

    fixed pycodestyles

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,16 +7,19 @@ from os import getenv
 from models import storage
 from api.v1.views import app_views
 
+
 app = Flask(__name__)
 CORS(app, origins='0.0.0.0')
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown(self):
-  """close session"""
-  storage.close()
+    """close session"""
+    storage.close()
+
 
 if __name__ == '__main__':
-  api_host = getenv('HBNB_API_HOST', default='0.0.0.0')
-  api_port = getenv('HBNB_API_PORT', default=5000)
-  app.run(host=api_host, port=int(api_port), threaded=True)
+    api_host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    api_port = getenv('HBNB_API_PORT', default=5000)
+    app.run(host=api_host, port=int(api_port), threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""
+"""status checking
 """
 from flask import jsonify
 
+
 @app_views.route("/status", strict_slashes=Flase)
 def status():
-  """returns status ok"""
-  return jsonify({'status': 'OK'})
+    """returns status ok"""
+    return jsonify({'status': 'OK'})
