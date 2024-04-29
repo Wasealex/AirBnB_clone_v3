c12f9398ba36b33dc676f6e2c5d07c84fb038379
ethioendu <117819326+ethioendu@users.noreply.github.com>
2024-04-29 14:31:12 +0300

    Update index.py by including new route /stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,27 @@
 """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
     """returns status ok"""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    stats = {}
+    classes = {
+        'Amenity': storage.count('Amenity'),
+        'BaseModel': storage.count('BaseModel'),
+        'City': storage.count('City'),
+        'Place': storage.count('Place'),
+        'Review': storage.count('Review'),
+        'State': storage.count('State'),
+        'User': storage.count('User')
+    }
+    for key, value in classes.items():
+        stats[key] = value
+    return jsonify(stats)
