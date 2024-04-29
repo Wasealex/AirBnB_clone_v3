06001cb785946b6e59fdaab35b4c138fa44858d2
Wasealex <wasealex27@gmail.com>
2024-04-29 13:14:20 +0300

    modifyed to make flask api work

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,24 +2,22 @@
 """set up flask app
 """
 from flask import Flask
-from flask_cors import CORS
 from os import getenv
 from models import storage
 from api.v1.views import app_views
 
 
 app = Flask(__name__)
-CORS(app, origins='0.0.0.0')
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
 @app.teardown_appcontext
-def teardown(self):
+def teardown_appcontext(exception):
     """close session"""
     storage.close()
 
 
 if __name__ == '__main__':
-    api_host = getenv('HBNB_API_HOST', default='0.0.0.0')
-    api_port = getenv('HBNB_API_PORT', default=5000)
-    app.run(host=api_host, port=int(api_port), threaded=True)
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,9 @@
 """status checking
 """
 from flask import jsonify
+from api.v1.views import app_views
 
-
-@app_views.route("/status", strict_slashes=Flase)
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
     """returns status ok"""
     return jsonify({'status': 'OK'})
